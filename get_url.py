45ba715daa3d3dfaa8ab2718e1c3097dccc611d6
kevinl355224 <134241836+kevinl355224@users.noreply.github.com>
2023-08-18 12:48:28 +0800

    Update get_url.py
    add ,

--- get_url.py
+++ get_url.py
@@ -1,6 +1,6 @@
 import requests
 
-def get_url(keyword,number):
+def get_url(keyword, number):
     number = int(number)
     page = 0
     url_list = []
@@ -8,11 +8,11 @@ def get_url(keyword,number):
         try :
             url_search_page = "https://www.104.com.tw/jobs/search/list?ro=0&kwop=7&keyword="+keyword+"&page="+str(page)
             headers = {
-                "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+                "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML,  like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36", 
                 "Referer":"https://www.104.com.tw/jobs/search/?keyword=%E6%9C%83%E8%A8%88&order=1&jobsource=2018indexpoc&ro=0"          
                 }
 
-            req = requests.get(url=url_search_page,headers=headers)
+            req = requests.get(url=url_search_page, headers=headers)
             data = req.json()
             for n in data["data"]["list"]:
                 # 不加入廣告內容
