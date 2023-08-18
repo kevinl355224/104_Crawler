715c15bcf68c5231562af4769e94da4fd74e44fe
kevinl355224 <134241836+kevinl355224@users.noreply.github.com>
2023-08-18 12:46:47 +0800

    Update crawl_detail.py
    add ,

--- crawl_detail.py
+++ crawl_detail.py
@@ -1,37 +1,37 @@
 import requests
 
-def crawl_detail(url,column_decide) ->list : 
+def crawl_detail(url, column_decide) ->list : 
     referer = url
     
     url2 = "https://www.104.com.tw/job/ajax/content/" +url.split("/")[-1]
 
 
     headers = {
-        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36",
+        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36", 
         "Referer":referer
         }
-    req = requests.get(url=url2,headers=headers)
+    req = requests.get(url=url2, headers=headers)
 
     data = req.json()
 
     data_list = [
-        ["1","更新日期","2023/06/23",'data["data"]["header"]["appearDate"]'],
-        ["2","職務名稱","會計",'data["data"]["header"]["jobName"]'],
-        ["3","薪資範圍","30000~40000",'data["data"]["jobDetail"]["salary"]'],
-        ["4","最低薪資","30000",'crawl_salary_min(data)'],
-        ["5","最高薪資","40000",'crawl_salary_max(data)'],
-        ["6","工作區域","大安區",'data["data"]["jobDetail"]["addressRegion"]'],
-        ["7","詳細地址","基隆路xx巷xx弄",'data["data"]["jobDetail"]["addressDetail"]'],
-        ["8","公司名稱","範例有限公司",'data["data"]["header"]["custName"]'],
-        ["9","工作性質","全職",'crawl_work_type(data)'],
-        ["10","上班時段","日班09:00~17:00",'data["data"]["jobDetail"]["workPeriod"]'],
-        ["11","休假制度","周休二日",'data["data"]["jobDetail"]["vacationPolicy"]'],
-        ["12","網址","https://xx",'referer']
+        ["1", "更新日期", "2023/06/23", 'data["data"]["header"]["appearDate"]'], 
+        ["2", "職務名稱", "會計", 'data["data"]["header"]["jobName"]'], 
+        ["3", "薪資範圍", "30000~40000", 'data["data"]["jobDetail"]["salary"]'], 
+        ["4", "最低薪資", "30000", 'crawl_salary_min(data)'], 
+        ["5", "最高薪資", "40000", 'crawl_salary_max(data)'], 
+        ["6", "工作區域", "大安區", 'data["data"]["jobDetail"]["addressRegion"]'], 
+        ["7", "詳細地址", "基隆路xx巷xx弄", 'data["data"]["jobDetail"]["addressDetail"]'], 
+        ["8", "公司名稱", "範例有限公司", 'data["data"]["header"]["custName"]'], 
+        ["9", "工作性質", "全職", 'crawl_work_type(data)'], 
+        ["10", "上班時段", "日班09:00~17:00", 'data["data"]["jobDetail"]["workPeriod"]'], 
+        ["11", "休假制度", "周休二日", 'data["data"]["jobDetail"]["vacationPolicy"]'], 
+        ["12", "網址", "https://xx", 'referer']
     ]
 
     ##### 有機會再增加#####
     # 不拘就不顯示
-    # condition_list = ["工作經驗","學歷要求","科系要求","擅長工具","工作技能","具備證照"]
+    # condition_list = ["工作經驗", "學歷要求", "科系要求", "擅長工具", "工作技能", "具備證照"]
     # data_dict["條件要求"] = data["data"]["condition"]
 
 
